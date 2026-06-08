53a9b4aaefeb97a1c16d43ddbc4206bd6498c48c
Wessel de Vries <wesseldvrs@gmail.com>
2026-06-08 21:41:10 +0200

    alsdkgj

--- python_backend/main.py
+++ python_backend/main.py
@@ -62,4 +62,4 @@ async def upload_csv(
         map_obj = generator.save_map(map.cmap, map.categorical)
         map_obj.save(str(PUBLIC_DIR / map.filename))
 
-    return f"{[map.filename for map in maps]} successfully uploaded to website."
+    return f"CSV successfully uploaded to website. Maps {[map.filename for map in maps]} succesfully generated."
